0e5c8b085f28c5dcec859f8d98080a15768cd1c9
lionadi <adrian.simionescu82@gmail.com>
2014-12-12 16:29:28 +0200

    New UI additions

--- MyTrainerTracker/app/src/main/java/com/example/extsimionescu/mytrainertracker/MainActivity.java
+++ MyTrainerTracker/app/src/main/java/com/example/extsimionescu/mytrainertracker/MainActivity.java
@@ -4,14 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+    Button bCreateSet;
+
+    EditText etExcersiceOneName;
+    EditText etExcersiceTwoName;
+    EditText etExcersiceThreeName;
+
+    EditText etTargetRepetitionsExcersiceOne;
+    EditText etTargetRepetitionsExcersiceTwo;
+    EditText etTargetRepetitionsExcersiceThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bCreateSet = (Button)findViewById(R.id.bCreateSet);
+        etExcersiceOneName   = (EditText)findViewById(R.id.etExcersiceOneName);
+        etExcersiceTwoName   = (EditText)findViewById(R.id.etExcersiceTwoName);
+        etExcersiceThreeName   = (EditText)findViewById(R.id.etExcersiceThreeName);
+
+        etTargetRepetitionsExcersiceOne   = (EditText)findViewById(R.id.etTargetRepetitionsExcersiceOne);
+        etTargetRepetitionsExcersiceTwo   = (EditText)findViewById(R.id.etTargetRepetitionsExcersiceTwo);
+        etTargetRepetitionsExcersiceThree   = (EditText)findViewById(R.id.etTargetRepetitionsExcersiceThree);
+
+        bCreateSet.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        TextView myTextView =
+                                (TextView)findViewById(R.id.status);
+                        myTextView.setText("Button clicked" + etExcersiceOneName.getText());
+                    }
+                }
+        );
     }
 
 
@@ -36,4 +67,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
